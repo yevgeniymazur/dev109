84e549a67da75630f1c56ef36ba307f1d7b2fb7d
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-02-08 17:09:23 -0800

    rule.js

--- m05/rule.js
+++ m05/rule.js
@@ -1,1 +1,16 @@
+var table = parseInt(prompt("Enter a number for the multiplication table:")); // Get user input
+var operator = 'multiplication'; // Type of calculation
+var i = 1; // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>'; // Message
 
+if (operator === 'multiplication') {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
