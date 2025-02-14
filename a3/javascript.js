1c2f791240644f5c36ef88b026c4f5c9567e4792
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-02-13 19:30:03 -0800

    javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -66,57 +66,45 @@ document.getElementById("upRight").innerHTML = rLine;
 }
 
 function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=pHeight; i >= 1;i--){
-rLine +="<p>";
-
-//Fill in the left side of every row
-for (x=1; x<=pHeight-i; x++){
-rLine +='';
-rLine +="<span class='space'>" + pSymbol +"</span>";
-}
-
-//Create each line on the Rhombus
-for(j=x;j<=i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("downLeft").innerHTML = rLine;
+  rLine = "";
+  for (i = pHeight; i >= 1; i--){
+    rLine += "<p>";
+    
+    // Add left padding: (pHeight - i) space spans
+    for (x = 1; x <= pHeight - i; x++){
+      rLine += "<span class='space'>" + pSymbol + "</span>";
+    }
+    
+    // Print the row's symbols (i symbols)
+    for (j = 1; j <= i; j++){
+      // Check even/odd
+      if (j % 2)
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    
+    rLine += "</p>";
+  }
+  
+  document.getElementById("downLeft").innerHTML = rLine;
 }
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=pHeight;i > 0; i--){
-rLine +="<p>";
-
-//Create each line on the Rhombus
-for(j=0;j<i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("downRight").innerHTML = rLine;
+  rLine = "";
+  for (i = pHeight; i > 0; i--){
+    rLine += "<p>";
+    
+    // No left padding here; symbols are printed from the left
+    for (j = 0; j < i; j++){
+      if (j % 2)
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    
+    rLine += "</p>";
+  }
+  
+  document.getElementById("downRight").innerHTML = rLine;
 }
